62d4f8e8685a7b8b87c694002622cd0537f4e7db
drlf <drlf@163.com>
2015-03-09 23:48:27 +0800

    添加test配置和脚本

---
+++ test/unit/controllersSpec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+xdescribe('controllers', function(){
+  beforeEach(module('myApp.controllers'));
+
+
+  it('should ....', inject(function() {
+    //spec body
+  }));
+
+  it('should ....', inject(function() {
+    //spec body
+  }));
+});
